4ba9429d5ac6c77f1d3dde3943f2c9b75de8f9f2
Michelle <Michelle.Tinajero@outlook.com>
2020-03-14 17:21:26 -0500

    Initial commit retake

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,20 +1,20 @@
-export const getSmurf = (data) => {
-    return {
-        type: 'GET_SMURF',
-        payload: data,
-    }
-}
+// export const getSmurf = (data) => {
+//     return {
+//         type: 'GET_SMURF',
+//         payload: data,
+//     }
+// }
 
-export const addSmurf = (data) => {
-    return {
-        type: "ADD_SMURF",
-        payload: data,
-    }
-}
+// export const addSmurf = (data) => {
+//     return {
+//         type: "ADD_SMURF",
+//         payload: data,
+//     }
+// }
 
-export const deleteSmurf = (data) => {
-    return {
-        type: "DELETE_SMURF",
-        payload: data,
-    }
-}
+// export const deleteSmurf = (data) => {
+//     return {
+//         type: "DELETE_SMURF",
+//         payload: data,
+//     }
+// }
